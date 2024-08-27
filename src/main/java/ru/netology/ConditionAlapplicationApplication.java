b258f3596dc107eda9e40520a1ef6ba5c21e7988
Vera Knyazeva <verashibanova@yandex.ru>
2024-08-27 16:53:14 +0500

    first commit

---
+++ src/main/java/ru/netology/ConditionAlapplicationApplication.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionAlapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConditionAlapplicationApplication.class, args);
+    }
+
+}
